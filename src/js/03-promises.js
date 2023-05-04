7d8655130f53a3e95f8c27badffa4efd44af23f4
Vladislavv99 <vladlegur6@gmail.com>
2023-05-04 13:40:13 +0300

    3 proces

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const btn = document.querySelector('.btn-3ex');
+
+const firstDelay = form.elements.delay;
+const step = form.elements.step;
+const amount = form.elements.amount;
+
+btn.addEventListener('click', e => {
+  e.preventDefault();
+  for (let i = 1; i <= amount.value; i++) {
+    const delay = i === 1 ? Number(firstDelay.value) : Number(firstDelay.value) = (i - 1) * Number(step.value);
+    createPromise(i, delay).then(value => {
+      Notiflix.Notify.success(value);
+    }).catch(err => {
+      Notiflix.Notify.failure(err);
+    }); 
+  }
+})
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
